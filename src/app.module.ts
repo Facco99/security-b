afc75fe06f6a22b1f5daa57c6c314222a9c2957a
majkl zumberi <majklzumberi00@gmail.com>
2021-03-03 14:17:52 +0100

    feat(auth): :construction: begin auth strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import { Article } from './article/entities/article.entity';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-  }),
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: process.env.DB_user,
-    password: process.env.DB_password,
-    database: 'sicurezza',
-    entities: [],
-    synchronize: true,
-    keepConnectionAlive: true,
-  }),
-    ArticleModule,],
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_user,
+      password: process.env.DB_password,
+      database: 'sicurezza',
+      entities: [Article, User],
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+    ArticleModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
